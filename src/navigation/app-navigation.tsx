a1b9dc4a0d009a3025ed74bb1f3a201874454edc
Jaume de Juan <jaume.dejuan@lingokids.com>
2026-08-19 18:01:07 +0200

    Fix ESlint warnings and errors

--- src/navigation/app-navigation.tsx
+++ src/navigation/app-navigation.tsx
@@ -1,45 +1,56 @@
-import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  BottomTabBar,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import {
   createStaticNavigation,
   type StaticParamList,
-} from '@react-navigation/native';
-import * as Linking from 'expo-linking';
-import { Platform } from 'react-native';
+} from "@react-navigation/native";
+import * as Linking from "expo-linking";
+import { Platform } from "react-native";
 
-import { ExploreScreen } from '@/features/explore/explore-screen';
-import { HomeScreen } from '@/features/home/home-screen';
+import { ExploreScreen } from "@/features/explore/explore-screen";
+import { HomeScreen } from "@/features/home/home-screen";
 
-import { WebTabBar } from './web-tab-bar';
+import { WebTabBar } from "./web-tab-bar";
 
 const RootTabs = createBottomTabNavigator({
-  initialRouteName: 'Home',
+  initialRouteName: "Home",
   screenOptions: {
     headerShown: false,
   },
   tabBar: (props) =>
-    Platform.OS === 'web' ? <WebTabBar {...props} /> : <BottomTabBar {...props} />,
+    Platform.OS === "web" ? (
+      <WebTabBar {...props} />
+    ) : (
+      <BottomTabBar {...props} />
+    ),
   screens: {
     Home: {
       screen: HomeScreen,
-      linking: '',
+      linking: "",
       options: {
-        tabBarLabel: 'Home',
+        tabBarLabel: "Home",
       },
     },
     Explore: {
       screen: ExploreScreen,
-      linking: 'explore',
+      linking: "explore",
       options: {
-        tabBarLabel: 'Explore',
+        tabBarLabel: "Explore",
       },
     },
   },
 });
 
-export interface RootTabParamList extends StaticParamList<typeof RootTabs> {}
+export type RootTabParamList = StaticParamList<typeof RootTabs>;
 
 const Navigation = createStaticNavigation(RootTabs);
 
 export function AppNavigation(): React.JSX.Element {
-  return <Navigation linking={{ enabled: 'auto', prefixes: [Linking.createURL('/')] }} />;
+  return (
+    <Navigation
+      linking={{ enabled: "auto", prefixes: [Linking.createURL("/")] }}
+    />
+  );
 }
